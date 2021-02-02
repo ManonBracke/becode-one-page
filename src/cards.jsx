ddc488f779f149c553b420a0958771dc5aff2922
ManonBracke <69969004+ManonBracke@users.noreply.github.com>
2021-02-02 16:47:56 +0100

    Finish

---
+++ src/cards.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const Welcome = () => {
+    return <div className="cards">
+        <div className="card">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            <h3>Apprendre à apprendre</h3>
+            <p>Chez BeCode, on apprend pas un language en particulier, on apprend à apprendre.</p>
+        </div>
+        <div className="card">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            <h3>Pédagogie active</h3>
+            <p>Pas de cours magistraux embêtants et dont on ne retient rien. Ici, la pédagogie est active.</p>
+        </div>
+        <div className="card">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            <h3>Ouvert à tou.t.e.s</h3>
+            <p>Votre genre, votre religion, votre couleur de peau, votre âge, vos diplômes, ON S'EN FOUT !</p>
+        </div>
+        <div className="card">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            <h3>Gratuit</h3>
+            <p>La formation est entièrement gratuite. Si vous n'avez pas de pc, on vous en prêtre un.</p>
+        </div>
+        <div className="card">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            <h3>Intensifs</h3>
+            <p>Du lundi au vendredi, de 9h00 à 17h00, vous apprendrez comme si vous étiez en entreprise.</p>
+        </div>
+    </div>;
+};
+
+export default Welcome;
---
+++ src/welcome.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+const Welcome = () => {
+    return <h1>Bienvenue chez BeCode. La nouvelles formation en horaire de jour pour apprendre les technologies numériques. Gratuitement.</h1>;
+};
+
+export default Welcome;
